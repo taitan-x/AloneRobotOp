434ebe5b4225178333e645ea0f51864d9ab4e247
AloneXBot <132041403+AloneXBot@users.noreply.github.com>
2023-06-20 08:46:30 +0530

    Update config.py

--- AloneRobot/config.py
+++ AloneRobot/config.py
@@ -47,7 +47,7 @@ class Config(object):
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list("elevated_users.json", "6079943111")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = "https://t.me/the_vip_boy" # EG, paypal
+    DONATION_LINK = "https://t.me/ALONE_WAS_BOT" # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
